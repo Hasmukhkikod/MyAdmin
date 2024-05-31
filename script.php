7756726dd9b3f939f1ba996826ea9d89a41f2083
Hasmukh kikod <163880543+Hasmukhkikod@users.noreply.github.com>
2024-05-31 16:10:21 +0530

    Add files via upload

---
+++ script.php
@@ -0,0 +1,11 @@
+<script src="<?php echo URL; ?>/assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="<?php echo URL; ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo URL; ?>/assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="<?php echo URL; ?>/assets/vendor/echarts/echarts.min.js"></script>
+  <script src="<?php echo URL; ?>/assets/vendor/quill/quill.js"></script>
+  <script src="<?php echo URL; ?>/assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="<?php echo URL; ?>/assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="<?php echo URL; ?>/assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?php echo URL; ?>/assets/js/main.js"></script>
---
+++ users.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Components / Accordion - NiceAdmin Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <?php 
+
+        //include('css.php');
+        require('css.php');
+    ?>
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Updated: Apr 20 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+    <?php include('header.php'); ?>
+  <!-- End Header -->
+
+  <!-- ======= Sidebar ======= -->
+    <?php include('aside.php'); ?>
+    <!-- End Sidebar-->
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>User</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item">Pages</li>
+          <li class="breadcrumb-item active">Blank</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-6">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Example Card</h5>
+              <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-lg-6">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Example Card</h5>
+              <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php require('footer.php'); ?>
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <?php require('script.php') ?>
+
+</body>
+
+</html>
